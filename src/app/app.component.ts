8ccc37b662ef61d2079683df379e8cc6759c1481
andre-carbajal <carbajalvargasandre@gmail.com>
2024-03-22 19:01:14 -0500

    Add RouterModule to imports in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EmployeeListComponent],
+  imports: [RouterOutlet, RouterModule, EmployeeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
